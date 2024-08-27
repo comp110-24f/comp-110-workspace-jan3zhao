85fa5e31cd4be865ed9c482bb0e0775a6d397bc3
Jane Zhao <jane.zizhen.zhao@gmail.com>
2024-08-27 13:07:16 -0400

    Finished Exercise 00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,11 @@
+"""My first exercise in COMP110!"""
+
+__author__ = "730776252"
+
+
+def greet(name: str) -> str:
+    """A welcoming first function definiton."""
+    return "Hello, " + name + "!"
+
+
+print(greet("Jane"))
